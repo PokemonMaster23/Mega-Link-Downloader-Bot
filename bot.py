d43f7bef6078c0fda2a46f0f4d0735f27dfb03c9
Pokemon Master <129300006+PokemonMaster23@users.noreply.github.com>
2023-03-29 19:49:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,9 @@
 python
-import os
-import re
-import requests
-from mega import Mega
-from telethon import TelegramClient, events
-from telethon.errors.rpcerrorlist import PeerIdInvalidError
-from telethon.tl.types import DocumentAttributeFilename
-
-# Import configuration variables from config.py
-from config import API_ID, API_HASH, BOT_TOKEN
-
-# Initialize the Telegram client and the Mega client
-client = TelegramClient('mega_downloader', API_ID, API_HASH)
-mega = Mega()
-
-# Pattern for detecting Mega.nz links in messages
-mega_link_pattern = re.compile(r'https?://mega.nz/(\w+)#(\w+)|https?://mega.nz/(\w+)')
-
-# Handler function for the '/start' command
-@client.on(events.NewMessage(pattern='/start'))
-async def start_handler(event):
-    await event.reply('Hi! I can download files from Mega.nz links. Just send me the link and I will download the file and send it to you!')
+def start_handler(event):
+    client.send_message(event.chat_id, 'Hi! I can download files from Mega.nz links. Just send me the link and I will download the file and send it to you!')
 
 # Handler function for files sent to the bot
-@client.on(events.NewMessage(func=lambda event: True))
-async def download_handler(event):
+def download_handler(event):
     # Check if the message has a mega.nz link
     message_text = event.message.message
     mega_match = mega_link_pattern.search(message_text)
@@ -36,7 +15,7 @@ async def download_handler(event):
     file_key = mega_match.group(2)
 
     # Download the file from Mega.nz
-    await event.reply('Downloading file...')
+    client.send_message(event.chat_id, 'Downloading file...')
     mega_file = mega.get_public_link(file_id, file_key)
     file_name = mega_file['name']
     file_data = requests.get(mega_file['url']).content
@@ -46,11 +25,13 @@ async def download_handler(event):
         f.write(file_data)
     attributes = [DocumentAttributeFilename(file_name)]
     try:
-        await client.send_file(event.chat_id, file_name, attributes=attributes)
+        client.send_file(event.chat_id, file_name, attributes=attributes)
     except PeerIdInvalidError:
-        await event.reply('Error sending file: Invalid chat ID')
+        client.send_message(event.chat_id, 'Error sending file: Invalid chat ID')
 
 # Start the bot
-client.start(bot_token=BOT_TOKEN)
+client.connect()
+client.add_event_handler(start_handler, events.NewMessage(pattern='/start'))
+client.add_event_handler(download_handler, events.NewMessage(func=lambda event: True))
+client.start()
 client.run_until_disconnected()
-```
