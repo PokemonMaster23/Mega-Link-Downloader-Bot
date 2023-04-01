bb171a3b6fd802d4f6be5b12f070fc3f6d4a3024
Pokemon Master <129300006+PokemonMaster23@users.noreply.github.com>
2023-04-01 18:59:41 +0530

    Completely rewritten in pyrogram! bot moved from betav1 => beta v2
    added mega email and password!

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-TOKEN = "your_bot_token_here"
-MEGA_EMAIL = 'your_mega_email'
-MEGA_PASSWORD = 'your_mega_password'
+API_ID = your_api_id
+API_HASH = 'your_api_hash'
+BOT_TOKEN = 'your_bot_token'
+EMAIL = 'your_mega_email'
+PASSWORD = 'your_mega_password'
