bc00e76783b84bef945ed5633f2a18cc6f8ee5fb
Pokemon Master <129300006+PokemonMaster23@users.noreply.github.com>
2023-04-25 14:59:15 +0530

    Added multi user support
    Beta v2 => v2.5

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, types
 from pyrogram.types import Message
 import config
 import logging
+import time
+import threading
+import asyncio
 import os
 from mega import Mega
 
@@ -21,39 +24,76 @@ app = Client(
 async def start_handler(client: Client, message: Message):
     await message.reply_text('Hello! This bot can download files from mega.nz')
 
+
+# List to store download tasks
+download_tasks = []
+
+# Callback function for progress of document upload
+async def progress_callback(current: int, total: int, message: Message):
+    progress = current * 100 / total
+    logging.info(f"Uploading document: {message.document.file_name} - {progress:.2f}%")
+
 # Download command handler
 @app.on_message(filters.command("download"))
 async def download_handler(client: Client, message: Message):
     # Check if link is provided
     if not message.text.split(" ")[1:]:
         await message.reply_text('Please provide a valid mega.nz link')
-        return
+        return None
     
     link = message.text.split(" ")[1]
     logging.info(f'Downloading link: {link}')
     await message.reply_text(f'Downloading {link}...')
 
-    # Login to Mega.nz
+
+    # Create a new thread for downloading
+    download_thread = threading.Thread(target=download_task, args=(link, client,message))
+    download_thread.start()
+
+    # Add task to list
+    download_tasks.append(link)
+    logging.info(f'Download tasks: {download_tasks}')
+
+    print(download_tasks)
+
+
+# Download
+def download_task(link, client,message):
+    # Initialize mega.py
     mega = Mega()
-    m = mega.login(config.EMAIL, config.PASSWORD)
+    mega.login(config.EMAIL, config.PASSWORD)
 
     # Download file
-    file = m.download_url(link)
-    logging.info(f'Downloaded file: {file}')
-    await message.reply_text(f'Downloaded file: {file}')
+    md = mega.download_url(link)
     
-    # Upload file to Telegram
-    logging.info(f'Uploading file: {file}')
-    await message.reply_text(f'Uploading {file}...')
-    await client.send_document(
+     # Upload file to Telegram
+    logging.info(f'Uploading file: {md}')
+    message.reply_text(f'Uploading {md}... - @{message.chat.username}')
+    time.sleep(5)
+    # Send file to User
+    client.send_document(
         chat_id=message.chat.id,
-        document=file,
-        progress=progress_callback
+        document=md
     )
+    
 
-# Progress callback function for file upload
-async def progress_callback(current, total):
-    logging.info(f"Uploaded {current} out of {total} bytes ({current/total*100:.2f}%)")
+    # Remove file from local storage
+    os.remove(md)
+
+
+# List command handler
+@app.on_message(filters.command("list"))
+async def list_handler(client: Client, message: Message):
+    # Check if there are any files in the download list
+    if not download_tasks:
+        await message.reply_text('No files in the download list.')
+        return None
+    
+    # Send the list of files to the user
+    file_list = '\n'.join(download_tasks)
+    await message.reply_text(f'Download list:\n{file_list}')
+
+
 
 # Run bot
 app.run()
